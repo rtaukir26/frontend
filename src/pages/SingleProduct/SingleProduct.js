354b693d095f03025cb2791fdc36907790ccdce6
rtaukir26 <111485623+rtaukir26@users.noreply.github.com>
2023-04-02 13:02:45 +0530

    Inc/dec qty done

--- src/pages/SingleProduct/SingleProduct.js
+++ src/pages/SingleProduct/SingleProduct.js
@@ -1,66 +1,56 @@
 import Header from "../../component/Header/Header";
-import RatingStar from "../../component/RatingStar/RatingStar";
-import watchImg from "../../assets/images/watch.webp";
+// import RatingStar from "../../component/RatingStar/RatingStar";
+// import watchImg from "../../assets/images/watch.webp";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getSingleProductApi } from "../../service/dashBoard";
+import ProductCard from "../../component/ProductCard/ProductCard";
 
 const SingleProduct = () => {
-  const [productInfo, setProductInfo] = useState({});
+  const [productInfo, setProductInfo] = useState({
+    loading: true,
+    error: "",
+    product: {},
+  });
   const { id } = useParams();
 
   useEffect(() => {
     getSingleProductApi(id)
       .then((res) => {
+        // console.log("productInfo res", res);
         if (res?.status === 200) {
-          setProductInfo(res);
+          setProductInfo({
+            loading: false,
+            product: { ...res?.data?.product, quantities: 1 },
+            error: "",
+          });
+        } else if (res?.message === "Network Error") {
+          setProductInfo({
+            loading: true,
+            product: {},
+            error: "Network Error",
+          });
+        } else {
+          setProductInfo({
+            loading: true,
+            product: {},
+            error: "Something went wrong!",
+          });
         }
       })
       .catch((err) => {
         return err;
+        // console.log("err",err);
       });
   }, []);
+  console.log("productInfo", productInfo);
 
   return (
     <section className="main_sec">
       <div className="body_outer_div">
         <div className="body_inner_div">
           <Header />
-          <div className="product_details_div ">
-            <div className="product_card">
-              <div className="card_left_img_div">
-                <img src={watchImg} alt="watch" />
-              </div>
-              <div className="card_right_product_info">
-                <h5>name</h5>
-                <p>category</p>
-                <p>Sescription</p>
-                <RatingStar value={4} />
-                <p>
-                  <strong>Price &#8377;</strong>
-                  54652
-                </p>
-                <div className="quantity_div">
-                  <strong>Quantity</strong>
-                  <span>-</span>
-                  <span>2</span>
-                  {/* <input
-                    type="number"
-                    readOnly
-                    value={2}
-                    style={{ width: "50px" }}
-                  /> */}
-                  <span>+</span>
-                  <strong>Total Price &#8377;7656</strong>
-                </div>
-
-                <div className="btn_grp">
-                  <button>Add to cart</button>
-                  <button>Buy now</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ProductCard setProductInfo={setProductInfo} productInfo={productInfo} />
         </div>
       </div>
     </section>
